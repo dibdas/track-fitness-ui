3ec2f1450c27a8792b77caae6fd60de485dd0ddf
dibdas <dibyandu08das@gmail.com>
2021-09-07 00:59:55 +0530

    modified

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,14 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
 import { Component } from 'react';
+import axios from 'axios';
 
 class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
+      password_digest: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,6 +22,21 @@ class SignIn extends Component {
   }
 
   handleSubmit=(event) => {
+    axios.post('http://localhost:3000/signin',
+      {
+        user: {
+          name,
+          password_digest,
+        },
+      }, { withCredentials: true }).then((response) => {
+      if (response.data.status === 'created') {
+        this.handleSuccessfulAuth(response.data);
+      }
+    })
+      .catch((error) => {
+        console.log('signin error', error);
+      });
+    event.preventDefault();
     event.preventDefault();
   }
 
@@ -28,6 +47,7 @@ class SignIn extends Component {
         <h1>Sign up</h1>
         <form onSubmit={this.handleSubmit}>
           <input placeholder="name" type="text" value={name} onChange={this.handleChange} />
+          <input placeholder="password_digest" type="text" value={password_digest} onChange={this.handleChange} />
           <button type="submit"> submit</button>
         </form>
       </div>
