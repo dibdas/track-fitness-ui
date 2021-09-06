469bea6bc6cc6b26d53a2163b4f50a683ad5f078
dibdas <dibyandu08das@gmail.com>
2021-09-06 18:58:07 +0530

    added signuo component

---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+    };
+  }
+}
+export default SignIn
