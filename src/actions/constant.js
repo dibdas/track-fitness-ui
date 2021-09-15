ff19b4e807c9230c781ffa34a51f1941935fb83b
dibdas <dibyandu08das@gmail.com>
2021-09-15 23:58:50 +0530

    added constant

--- src/actions/constant.js
+++ src/actions/constant.js
@@ -1,25 +1,2 @@
-const ERROR_MEALS = 'ERROR_MEALS';
-const SUCCESS_MEALS = 'SUCCESS_MEALS';
-const SUCCESS_MEAL = 'SUCCESS_MEAL';
-const IDLE_MEALS = 'IDLE_MEALS';
-const CHANGE_CATEGORY = ' CHANGE_CATEGORY';
-const successmeals = (data) => ({
-  type: 'SUCCESS_MEALS',
-  data,
-});
-
-const successmeal = (data) => ({
-  type: 'SUCCESS_MEAL',
-  data,
-});
-
-const errormeals = (error) => ({
-  type: 'ERROR_MEALS',
-  error,
-});
-const changecategory = (category) => ({
-  type: 'CHANGE_CATEGORY',
-  category,
-
-});
-export {
+export const Exercise = 'EXERCISE';
+export const Measurement = 'MEASUREMENT';
