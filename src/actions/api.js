2bff76484b67c5fb5e3f04a8f4d3436ab070463a
dibdas <dibyandu08das@gmail.com>
2021-09-15 23:37:12 +0530

    add api.js

---
+++ src/actions/api.js
@@ -0,0 +1,43 @@
+const exercise = async (id) => {
+  const url = 'http://localhost:3000/exercise/id';
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  }
+
+  throw Error(404);
+};
+
+const measurement = async (id) => {
+  const url = 'http://localhost:3000/measurements/id';
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  }
+
+  throw Error(404);
+};
+
+const postmeasurement = async (id) => {
+  try {
+    const response = await fetch('http://localhost:3000/measurements/id', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+      }),
+    });
+    const data = await response.json();
+    // enter you logic when the fetch is successful
+    console.log(data);
+  } catch (error) {
+    // enter your logic for when there is an error (ex. error toast)
+
+    console.log(error);
+  }
+};
+
+export { measurement, exercise,postmeasurement };
--- src/components/ExerciseForm.js
+++ src/components/ExerciseForm.js
@@ -1,4 +1,9 @@
 /* eslint-disable react/destructuring-assignment */
+
+import PropTypes from 'prop-types';
+
+
+
 const ExeciseForm = (measurements, exercise, measureChange) => (
   <div>
     <h4>{exercise.name}</h4>
@@ -6,4 +11,11 @@ const ExeciseForm = (measurements, exercise, measureChange) => (
   </div>
 
 );
+ExeciseForm.propTypes = {
+  measureChange = PropTypes.func.isRequired,
+  exercise= PropTypes.objectOf(PropTypes.any).isRequired,
+  measurements=PropTypes.objectOf(PropTypes.array).isRequired,
+
+
+};
 export default ExeciseForm;
