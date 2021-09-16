579da11bfc75faa4c876e328023c63c70677b075
dibdas <dibyandu08das@gmail.com>
2021-09-16 18:04:38 +0530

    modified

--- src/actions/constant.js
+++ src/actions/constant.js
@@ -1,2 +1,29 @@
-export const Exercise = 'EXERCISE';
-export const Measurement = 'MEASUREMENT';
+const ERROR_EXERCISE = 'ERROR_EXERCISE';
+const SUCCESS_EXERCISE = 'SUCCESS_EXERCISE';
+const SUCCESS_MEASUREMENT = 'SUCCESS_MEASUREMENT';
+const ERROR_MEASUREMENT = 'ERROR_MEASUREMENT';
+const successmeasure = (data) => ({
+  type: 'SUCCESS_MEASUREMENT',
+  data,
+});
+
+const successexercise = (data) => ({
+  type: 'SUCCESS_EXERCISE',
+  data,
+});
+
+const errorexercise = (error) => ({
+  type: 'ERROR_EXERCISE',
+  error,
+});
+
+const errormeasure = (error) => ({
+  type: 'ERROR_MEASUREMENT',
+  error,
+});
+
+export {
+  ERROR_EXERCISE, SUCCESS_EXERCISE,
+  SUCCESS_MEASUREMENT, ERROR_MEASUREMENT,
+  successexercise, successmeasure, errorexercise, errormeasure,
+};
