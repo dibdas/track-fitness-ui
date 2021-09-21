b5712f7a41ef85773b588e68ede1e7358e450108
dibdas <dibyandu08das@gmail.com>
2021-09-21 18:15:38 +0530

    added use State

--- src/containers/MeasurementContainer.js
+++ src/containers/MeasurementContainer.js
@@ -1,8 +1,28 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable import/no-named-as-default-member */
 import { connect } from 'react-redux';
-import { exercise, postmeasurement, measurement } from '../actions/api';
+import { PropTypes } from 'prop-types';
+import { useHistory } from 'react-router';
 import Measurement from '../components/Measurement';
+import { useState } from 'react';
+const Measurement=({exercises})=>{
+  const [leftbicep,setLeftBicep]=useState(0);
+  const [rightbicep,setRightBicep]=useState(0);
+  const [waist,setWaist] = useState(0);
+  const [hip,setHip] = useState(0);
+  const [leftthigh,setLeftThigh] = useState(0);
+  const [rightthigh,setRightThigh] = useState(0);
+  const [createdAt,setCreatedAt] = useState(0);
+
+  const handleDate=(e)=>{
+    setCreatedAt(e.target.value)
+    
+    };
+
+  const handleDate = e => {
+    setCreatedAt(e.target.value);
+  };
+
 
 const mapStateToProps = (state) => {
   state.measurements.data;
@@ -13,4 +33,5 @@ const mapDispatchToProps = {
   measurement: (id) => measurement(id),
 
 };
+}
 export default connect(mapDispatchToProps, mapStateToProps)(Measurement);
