1bd13d9f08886c5351612c4a1a6d593a1fd82834
dibdas <dibyandu08das@gmail.com>
2021-09-21 19:47:09 +0530

    added circular pogress bar

---
+++ src/components/Pogress.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const Pogress= ({ measurement, exercises }) =>
+
+return(
+<>
+
+  {exercises.map((exercise)=>(
+            exercise.id
+            ))}
+
+
+)
+
+  </>
+);
