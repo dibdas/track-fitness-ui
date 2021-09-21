aa6a480ed29bfe61fd08ebfaf322a63384f1a6d0
dibdas <dibyandu08das@gmail.com>
2021-09-21 19:47:31 +0530

    added navbar

--- src/containers/MeasurementContainer.js
+++ src/containers/MeasurementContainer.js
@@ -5,6 +5,7 @@ import { PropTypes } from 'prop-types';
 import { useHistory } from 'react-router';
 import Measurement from '../components/Measurement';
 import { useState } from 'react';
+import ExeciseForm from '../components/ExerciseForm';
 const Measurement=({exercises})=>{
   const [leftbicep,setLeftBicep]=useState(0);
   const [rightbicep,setRightBicep]=useState(0);
@@ -14,9 +15,6 @@ const Measurement=({exercises})=>{
   const [rightthigh,setRightThigh] = useState(0);
   const [createdAt,setCreatedAt] = useState(0);
 
-  const handleDate=(e)=>{
-    setCreatedAt(e.target.value)
-  };
   const setMeasurement = (e) =>{
     const {name,value} = e.target
     switch (name) {
@@ -32,21 +30,48 @@ const Measurement=({exercises})=>{
         return setRightThigh(value);
       case 'waist':
         return setWaist(value);
-
-
+        default :
+        return 0;
     }
   }
 
+  const handleDate=(e)=>{
+    setCreatedAt(e.target.value)
+  };
 
+  return (
+    <div>
+      <h4>Add Measurement</h4>
+      <div>
+      <input
+        type="date"
+        placeholder="enter the date"
+        name={createdAt}
+        onChange={(e) => { handleDate(e); }}
+      />
+      </div>
+      <ul>
+        {exercises.map(exercise =>
+        < ExeciseForm name={exercise.name} handleMeasurement ={(e)=>{setMeasurement(e)}}/>
+        )}
+      </ul>
+      <button type="button"></button>
 
-const mapStateToProps = (state) => {
-  state.measurements.data;
-};
-const mapDispatchToProps = {
-  getexercise: (id) => exercise(id),
-  getpostmeasurement: (id) => postmeasurement(id),
-  measurement: (id) => measurement(id),
-
-};
+    </div>
+  )
 }
+  
+
+
+Measurement.propTypes={
+  exercises:PropTypes.func.isRequired
+
+}
+const mapStateToProps = (state) => ({
+  exercises: state.exercise,
+});
+const mapDispatchToProps = {
+  
+};
+
 export default connect(mapDispatchToProps, mapStateToProps)(Measurement);
