7a446c8cf26872df2813315784c31db83b39d124
dibdas <dibyandu08das@gmail.com>
2021-09-24 01:59:17 +0530

    added execise

---
+++ src/actions/measurement.js
@@ -0,0 +1,40 @@
+export const SUCCESS_MEASUREMENT = 'SUCCESS_MEASUREMENT';
+export const ERROR_MEASUREMENT = 'ERROR_MEASUREMENT';
+export const FETCH_MEASUREMENT = 'FETCH_MEASUREMENT';
+export const ADD_MEASUREMENT_ERROR = 'ADD_MEASUREMENT_ERROR';
+
+export const addMeasurementsError = () => ({ type: ADD_MEASUREMENT_ERROR });
+
+export const successMeasurements = (measurements) => ({
+  type: SUCCESS_MEASUREMENT,
+  measurements,
+});
+export const errorMeasurements = () => ({
+  type: ERROR_MEASUREMENT,
+});
+
+export const fetchMeasurementsAsync = () => async (dispatch) => {
+  fetch('/measurement', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(((result) => {
+      if (result.status !== 200) {
+        return dispatch(errorMeasurements());
+      }
+    })
+      .then((data) => dispatch(successMeasurements(data)))
+      .catch(() => dispatch(errorMeasurements())));
+};
+export const addMeasurementsAsync = () => {
+  fetch('/measurement', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+};
+
+export const fetchingCategories = () => ({ type: FETCHING_CATEGORIES });
