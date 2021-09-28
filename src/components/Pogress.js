fd920b204b122517cfca8f23e98c3ede0ba8f38e
dibdas <dibyandu08das@gmail.com>
2021-09-28 19:07:32 +0530

    modified

--- src/components/Pogress.js
+++ src/components/Pogress.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { Redirect } from 'react-router-dom';
@@ -12,24 +12,25 @@ import { connect } from 'react-redux';
 import currentToken from '../containers/Tokenchecker';
 import { fetchMeasurementsAsync } from '../actions/measurement';
 
-const Pogress = ({ fetchMeasurements }) => {
+const Pogress = ({ measurements, fetchMeasurements }) => {
+  useEffect(() => {
+    fetchMeasurements();
+  }, []);
   if (currentToken() === null) {
     return <Redirect to="/login" />;
   }
-  console.log(fetchMeasurements);
+  console.log(measurements);
   return (
     <div>
-
-      {fetchMeasurements.map((measurement) => (
-
-        <div key={measurement.id}>
-          <div className="p-2">
-            {(measurement.created_at)}
-            <CircularProgressbar value={measurement.measure} />
+      {measurements !== undefined
+        ? measurements.map((measurement) => (
+          <div key={measurement.id}>
+            <div className="p-2">
+              {(measurement.created_at)}
+              <CircularProgressbar value={measurement.measure} />
+            </div>
           </div>
-        </div>
-      ))}
-
+        )) : <p>No measurements</p>}
     </div>
   );
 };
@@ -39,12 +40,12 @@ Pogress.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  measurements: state.measurements,
+  measurements: state.measurementReducer.measurements,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchMeasurement: (measure) => (
-    dispatch(fetchMeasurementsAsync(measure))
+  fetchMeasurements: () => (
+    dispatch(fetchMeasurementsAsync())
   ),
 });
 
